53b6376efa274649cdbe83478966a3b46907cfe6
Peter Fournier <pfournier@parkwd.com>
2019-06-28 11:25:26 -0400

    Added sample project

---
+++ samples/MySquad/MySquad/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using MySquad.Models;
+
+namespace MySquad.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
